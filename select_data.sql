929a436b798cae349d85fd44c0df21cdd6961096
Roman Khadraliev <Romch33tos@yandex.ru>
2026-07-02 16:38:27 +0700

    test: add data verification scripts with ordered selection

---
+++ select_data.sql
@@ -0,0 +1,7 @@
+﻿SELECT * FROM division ORDER BY id;
+SELECT * FROM employee ORDER BY id;
+SELECT * FROM vehicle ORDER BY id;
+SELECT * FROM route ORDER BY id;
+SELECT * FROM repair ORDER BY id;
+SELECT * FROM statistic ORDER BY id;
+SELECT * FROM assignment ORDER BY id;
